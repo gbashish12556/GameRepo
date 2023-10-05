97df1841973575c3f9b60926cac9a8ddcc071f1c
Ashish Kumar Gupta <ashishkg@tekion.com>
2023-10-05 16:31:22 +0530

    minor change

--- app/src/main/java/com/example/android/architecture/blueprints/machinecodingporoject/data/JsonDataSource.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/machinecodingporoject/data/JsonDataSource.kt
@@ -8,7 +8,7 @@ import com.google.gson.Gson
 class JsonDataSource(val context:Context):DataSource {
 
     override suspend fun getAllGames(): Response {
-        val text = context.resources.openRawResource(R.raw.Data)
+        val text = context.resources.openRawResource(R.raw.data)
             .bufferedReader().use { it.readText() }
 
         var response = Gson().fromJson<Response>(text, Response::class.java)
